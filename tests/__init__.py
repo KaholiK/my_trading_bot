71e5441e068cf52502b5f3b1000c07e482786a5c
KaholiK <131408085+KaholiK@users.noreply.github.com>
2025-10-02 19:16:19 -1000

    Delete tests directory

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# tests/__init__.py
-
-# This file can be left empty. It indicates that 'tests' is a Python package.
--- tests/test_dummy.py
+++
@@ -1,2 +0,0 @@
-def test_dummy():
-    assert True, "Dummy test to ensure testing setup is working."
